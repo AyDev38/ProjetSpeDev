db22eb600dd78786de57f6e52b0e51fe42351d3b
AymeRx <aymeric.surre@gmail.com>
2024-04-24 11:39:48 +0200

    Change port for running back-end

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const session = require('express-session');
 const SQLiteStore = require('connect-sqlite3')(session);
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
