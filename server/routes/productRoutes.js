fcfa0515288356b727132c79a5180dd2ac80fa26
AymeRx <aymeric.surre@gmail.com>
2024-04-26 12:05:30 +0200

    Add Cart to bdd + routes cart in back end

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -3,6 +3,7 @@ const Product = require('../models/product');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+const Cart = require('../models/cart');
 const { Op } = require('sequelize');
 
 router.post('/', async (req, res) => {
@@ -96,5 +97,58 @@ router.delete('/:id', authenticateToken, async (req, res) => {
       res.status(500).send(error);
   }
 });
+
+router.post('/add-to-cart', authenticateToken, async (req, res) => {
+  try {
+      const product = await Product.findByPk(req.body.productId);
+      if (product) {
+          let cart = await Cart.findOne({ where: { userId: req.user.userId } });
+          if (!cart) {
+              cart = await Cart.create({ userId: req.user.userId });
+          }
+          await cart.addProduct(product, { through: { quantity: req.body.quantity } });
+          res.status(201).send({ message: 'Product added to cart.' });
+      } else {
+          res.status(404).send({ message: 'Product not found!' });
+      }
+  } catch (error) {
+      res.status(500).send(error);
+  }
+}
+);
+
+router.get('/cart', authenticateToken, async (req, res) => {
+  try {
+      const cart = await Cart.findOne({ where: { userId: req.user.userId }, include: Product });
+      if (cart) {
+          res.status(200).send(cart);
+      } else {
+          res.status(404).send({ message: 'Cart not found!' });
+      }
+  } catch (error) {
+      res.status(500).send(error);
+  }
+}
+);
+
+router.delete('/cart/:productId', authenticateToken, async (req, res) => {
+  try {
+      const product = await Product.findByPk(req.params.productId);
+      if (product) {
+          const cart = await Cart.findOne({ where: { userId: req.user.userId } });
+          if (cart) {
+              await cart.removeProduct(product);
+              res.status(200).send({ message: 'Product removed from cart.' });
+          } else {
+              res.status(404).send({ message: 'Cart not found!' });
+          }
+      } else {
+          res.status(404).send({ message: 'Product not found!' });
+      }
+  } catch (error) {
+      res.status(500).send(error);
+  }
+}
+);
 
 module.exports = router;
